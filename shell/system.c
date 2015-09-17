f712cef2c8e2bdd7e03c13b273242354ec63adec
Jack Lange <jacklange@cs.pitt.edu>
2015-09-16 23:22:58 -0400

    added missing system files

---
+++ shell/system.c
@@ -0,0 +1,84 @@
+/* 
+ * Copyright (c) 2015, Jack Lange <jacklange@cs.pitt.edu>
+ * All rights reserved.
+ *
+ * This is free software.  You are permitted to use,
+ * redistribute, and modify it as specified in the file "PETLAB_LICENSE".
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <pet_log.h>
+
+#include <hobbes.h>
+#include <hobbes_system.h>
+
+int 
+list_memory_main(int argc, char ** argv)
+{
+    struct hobbes_memory_info * blk_arr = NULL;
+    uint32_t num_numa  = 0;
+    uint64_t num_blks  = 0;
+    uint64_t blk_size  = 0;
+
+    uint64_t total_mem = 0;
+    uint64_t free_mem  = 0;
+
+    uint64_t i = 0;
+    
+
+
+    num_numa  = hobbes_get_numa_cnt();
+    total_mem = hobbes_get_mem_size();
+    free_mem  = hobbes_get_free_mem();
+    blk_size  = hobbes_get_block_size();
+
+    blk_arr   = hobbes_get_memory_list(&num_blks);
+
+
+    if (num_blks == -1) {
+	ERROR("Could not retrieve memory list\n");
+	return -1;
+    }
+
+
+    printf("%lu memory blocks [block size=%lu] (%d numa domains)\n", num_blks, blk_size, num_numa);
+    printf("Total mem: %luMB ; Free Mem: %luMB\n", 
+	   total_mem / (1024 * 1024), 
+	   free_mem  / (1024 * 1024));
+
+    if (num_blks == 0) {
+	return 0;
+    }
+
+    printf("-------------------------------------------------------------------------------------\n");
+    printf("| Blk ID | Physical Address                 | Numa | Free | Enclave ID | App ID     |\n");
+    printf("-------------------------------------------------------------------------------------\n");
+
+    for (i = 0; i < num_blks; i++) {
+	printf("| %-*lu | %-*lu | %-*d | %-*d | %-*d | %-*d |\n",
+	       6,  i,
+               32, blk_arr[i].base_addr,
+	       4,  blk_arr[i].numa_node,
+	       4,  blk_arr[i].free,
+               10, blk_arr[i].enclave_id,
+               10, blk_arr[i].app_id);
+    }
+
+    printf("-------------------------------------------------------------------------------------\n");
+    
+    free(blk_arr);
+
+    return 0;
+}
+
+
+
+int 
+list_cpus_main(int argc, char ** argv)
+{
+
+    return 0;
+}
---
+++ shell/system.h
@@ -0,0 +1,16 @@
+/* 
+ * Copyright (c) 2015, Jack Lange <jacklange@cs.pitt.edu>
+ * All rights reserved.
+ *
+ * This is free software.  You are permitted to use,
+ * redistribute, and modify it as specified in the file "PETLAB_LICENSE".
+ */
+
+#ifndef __SYSTEM_H__
+#define __SYSTEM_H__
+
+
+int list_memory_main(int argc, char ** argv);
+int list_cpus_main(int argc, char ** argv);
+
+#endif
