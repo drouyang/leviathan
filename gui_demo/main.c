1a33bbebb3fa6d6d55fedf027e9a93d4e68380e7
Jack Lange <jacklange@cs.pitt.edu>
2015-10-23 15:06:58 -0400

    add skeleton for gui demo

---
+++ gui_demo/main.c
@@ -0,0 +1,32 @@
+/* 
+ * Copyright (c) 2015, Jack Lange <jacklange@cs.pitt.edu>
+ * All rights reserved.
+ *
+ * This is free software.  You are permitted to use,
+ * redistribute, and modify it as specified in the file "PETLAB_LICENSE".
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+
+#include <pet_log.h>
+#include <ezxml.h>
+
+#include <hobbes.h>
+
+
+bool use_topo_file = true;
+
+
+int main(int argc, char ** argv) {
+    
+
+    
+
+
+    return 0;
+}
