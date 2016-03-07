1324a908ab7743854f286c40a39c87872bcd1f95
Brian Kocoloski <briankoco@cs.pitt.edu>
2016-03-07 16:20:14 -0500

    Add hashtable of open connections to command queues. This is so the server can directly invoke xemem_signal() rather than xemem_signal_segid() on command completion. The latter causes performance problems that can be fixed by caching apids rather than lookinh them up on each command.

--- libhobbes/hobbes_cmd_queue.c
+++ libhobbes/hobbes_cmd_queue.c
@@ -60,22 +60,61 @@ typedef enum {
     HCQ_CLIENT  = 2} hcq_type_t;
 
 
+struct server_cmd_queue {
+    /* segid mapping the hcq (created by server/attached by client) */
+    xemem_segid_t      segid;
+
+    /* fd to poll for incoming commands */
+    int                fd;    
+
+    /* hashtable of command handlers */
+    struct hashtable * cmd_handlers;
+
+    /* hashtable of client connections 
+     * (maps client segid to server-attached apid)
+     */
+    struct hashtable * connections;
+};
+
+struct client_cmd_queue {
+    /* apid to attach/kick server */
+    xemem_apid_t       apid;
+
+    /* segid used to kick client upon command completion */
+    xemem_segid_t      segid;
+
+    /* fd to poll for command responses */
+    int                fd;
+};
+
 
 struct cmd_queue {
     hcq_type_t type; 
-    int fd;
 
-    xemem_segid_t client_segid;
-
+    /* Database memory mappings */
     void  * db_addr;
     void  * db;
 
-    xemem_segid_t segid;
-    xemem_apid_t  apid;
-
-    struct hashtable * cmd_handlers;
-
+    union {
+	struct server_cmd_queue server;
+	struct client_cmd_queue client;
+    };
 };
+
+
+static uint32_t
+handler_hash_fn(uintptr_t key)
+{
+    return pet_hash_ptr(key);
+}
+
+static int
+handler_eq_fn(uintptr_t key1,
+	      uintptr_t key2)
+{
+    return (key1 == key2);
+}
+
 
 
 
@@ -110,7 +149,10 @@ hcq_get_segid(hcq_handle_t hcq)
 {
     struct cmd_queue * cq = hcq;
 
-    return cq->segid;
+    if (cq->type != HCQ_SERVER)
+	return XEMEM_INVALID_SEGID;
+
+    return cq->server.segid;
 }
 
 int
@@ -118,30 +160,21 @@ hcq_get_fd(hcq_handle_t hcq)
 {
     struct cmd_queue * cq = hcq;
 
-    return cq->fd;
+    if (cq->type == HCQ_SERVER)
+	return cq->server.fd;
+    else if (cq->type == HCQ_CLIENT)
+	return cq->client.fd;
+    else
+	return -1;
 }
-
-
-static uint32_t
-handler_hash_fn(uintptr_t key)
-{
-    return pet_hash_ptr(key);
-}
-
-static int
-handler_eq_fn(uintptr_t key1,
-	      uintptr_t key2)
-{
-    return (key1 == key2);
-}
-
 
 hcq_handle_t
 hcq_create_queue(char * name) 
 {
-    struct cmd_queue * cq    = NULL;
-    struct hashtable * ht    = NULL;
-    xemem_segid_t      segid = 0;
+    struct cmd_queue * cq     = NULL;
+    struct hashtable * cmd_ht = NULL;
+    struct hashtable * seg_ht = NULL;
+    xemem_segid_t      segid  = 0;
 
     void * db_addr = NULL;
     void * db      = NULL;
@@ -168,13 +201,20 @@ hcq_create_queue(char * name)
     }
 
     /* Create the htable of handlers */
-    ht = pet_create_htable(0, handler_hash_fn, handler_eq_fn);
+    cmd_ht = pet_create_htable(0, handler_hash_fn, handler_eq_fn);
 
-    if (ht == NULL) {
+    if (cmd_ht == NULL) {
 	ERROR("Could not create hcq command hashtable\n");
-	goto ht_out;
+	goto cmd_ht_out;
     }
 
+    /* Create the htable of segids */
+    seg_ht = pet_create_htable(0, handler_hash_fn, handler_eq_fn);
+
+    if (seg_ht == NULL) {
+	ERROR("Could not create hcq connection hashtable\n");
+	goto seg_ht_out;
+    }
     
     cq = calloc(sizeof(struct cmd_queue), 1);
     if (!cq) {
@@ -182,22 +222,26 @@ hcq_create_queue(char * name)
 	goto cq_out;
     }
 
-    cq->type         = HCQ_SERVER;
-    cq->fd           = fd;
-    cq->db_addr      = db_addr;
-    cq->db           = db;
-    cq->segid        = segid;
-    cq->apid	     = 0;
-    cq->cmd_handlers = ht;
+    cq->type    = HCQ_SERVER;
+    cq->db_addr = db_addr;
+    cq->db      = db;
+
+    cq->server.segid        = segid;
+    cq->server.fd	    = fd;
+    cq->server.cmd_handlers = cmd_ht;
+    cq->server.connections  = seg_ht;
 
     init_cmd_queue(cq);
 
     return cq;
 
 cq_out:
-    pet_free_htable(ht, 0, 0);
+    pet_free_htable(seg_ht, 0, 0);
 
-ht_out:
+seg_ht_out:
+    pet_free_htable(cmd_ht, 0, 0);
+
+cmd_ht_out:
     xemem_remove(segid);
 
 segid_out:
@@ -219,9 +263,31 @@ hcq_free_queue(hcq_handle_t hcq)
 
     wg_delete_local_database(cq->db);
 
-    xemem_remove(cq->segid);
+    close(cq->server.fd);
+    xemem_remove(cq->server.segid);
 
-    pet_free_htable(cq->cmd_handlers, 0, 0);
+    /* Remove all apids from the connection table */
+    {
+	struct hashtable_iter * iter = NULL;
+
+	iter = pet_htable_create_iter(cq->server.connections);
+	if (iter == NULL) {
+	    ERROR("Could not create connections table iterator\n");
+	    goto out;
+	}
+
+	do {
+	    xemem_apid_t apid = (xemem_apid_t)pet_htable_get_iter_value(iter);
+	    xemem_release(apid);
+	} while (pet_htable_iter_remove(iter, 0) != 0);
+
+	pet_htable_free_iter(iter);
+    }
+
+out:
+    pet_free_htable(cq->server.connections, 0, 0);
+
+    pet_free_htable(cq->server.cmd_handlers, 0, 0);
 
     free(cq);
 
@@ -240,12 +306,12 @@ hcq_register_cmd(hcq_handle_t hcq,
 	return -1;
     }
 
-    if (pet_htable_search(cq->cmd_handlers, cmd_code) != 0) {
+    if (pet_htable_search(cq->server.cmd_handlers, cmd_code) != 0) {
 	ERROR("Attempted to register duplicate command handler (cmd=%lu)\n", cmd_code);
 	return -1;
     }
 
-    if (pet_htable_insert(cq->cmd_handlers, cmd_code, (uintptr_t)handler_fn) == 0) {
+    if (pet_htable_insert(cq->server.cmd_handlers, cmd_code, (uintptr_t)handler_fn) == 0) {
 	ERROR("Could not register hcq command (cmd=%lu)\n", cmd_code);
 	return -1;
     }
@@ -264,7 +330,7 @@ hcq_get_cmd_handler(hcq_handle_t hcq,
 	return NULL;
     }
 
-    return (hcq_cmd_fn)pet_htable_search(cq->cmd_handlers, (uintptr_t)hcq_get_cmd_code(hcq, cmd));
+    return (hcq_cmd_fn)pet_htable_search(cq->server.cmd_handlers, (uintptr_t)hcq_get_cmd_code(hcq, cmd));
 }
 
 hcq_handle_t
@@ -286,15 +352,14 @@ hcq_connect(xemem_segid_t segid)
     
     if (client_segid <= 0) {
 	ERROR("Could not create client signal segid\n");
-	return HCQ_INVALID_HANDLE;
+	goto out_xemem_make;
     }
 
     apid = xemem_get(segid, XEMEM_RDWR);
     
     if (apid <= 0) {
-	xemem_remove(client_segid);
 	ERROR("Failed to get APID for command queue\n");
-	return HCQ_INVALID_HANDLE;
+	goto out_xemem_get;
     }
 
     addr.apid   = apid;
@@ -304,9 +369,7 @@ hcq_connect(xemem_segid_t segid)
 
     if (db_addr == MAP_FAILED) {
 	ERROR("Failed to attach to command queue\n");
-	xemem_release(apid);
-	xemem_remove(client_segid);
-	return HCQ_INVALID_HANDLE;
+	goto out_xemem_attach;
     }
 
 
@@ -314,25 +377,33 @@ hcq_connect(xemem_segid_t segid)
 
     if (db == NULL) {
 	ERROR("Failed to attach command queue database\n");
-	xemem_detach(db_addr);
-	xemem_release(apid);
-	xemem_remove(client_segid);
-	return HCQ_INVALID_HANDLE;
+	goto out_db_attach;
     }
 
 
     cq = calloc(sizeof(struct cmd_queue), 1);
 
-    cq->type         = HCQ_CLIENT;
-    cq->fd           = client_fd;
-    cq->client_segid = client_segid;
-    cq->db_addr      = db_addr;
-    cq->db           = db;
-    cq->segid        = segid;
-    cq->apid         = apid;
-    cq->cmd_handlers = NULL;
+    cq->type    = HCQ_CLIENT;
+    cq->db_addr = db_addr;
+    cq->db      = db;
+
+    cq->client.fd    = client_fd;
+    cq->client.apid  = apid;
+    cq->client.segid = client_segid;
 
     return cq;
+
+out_db_attach:
+    xemem_detach(db_addr);
+
+out_xemem_attach:
+    xemem_release(apid);
+
+out_xemem_get:
+    xemem_remove(client_segid);
+
+out_xemem_make:
+    return HCQ_INVALID_HANDLE;
 }
 
 
@@ -341,13 +412,19 @@ hcq_disconnect(hcq_handle_t hcq)
 {
     struct cmd_queue * cq = hcq;
 
+    if (cq->type != HCQ_CLIENT) {
+	ERROR("Only clients can disconnect an HCQ\n");
+	return;
+    }
+
     wg_detach_local_database(cq->db);
 
     xemem_detach(cq->db_addr);
 
-    xemem_release(cq->apid);
+    xemem_release(cq->client.apid);
 
-    xemem_remove(cq->client_segid);
+    close(cq->client.fd);
+    xemem_remove(cq->client.segid);
 
     free(cq);
 
@@ -421,7 +498,7 @@ __cmd_issue(struct cmd_queue * cq,
     wg_set_field(db, cmd_rec, HCQ_TYPE_FIELD,             wg_encode_int(db, HCQ_CMD_TYPE));
     wg_set_field(db, cmd_rec, HCQ_CMD_FIELD_ID,           wg_encode_int(db, cmd_id));
     wg_set_field(db, cmd_rec, HCQ_CMD_FIELD_CMD_CODE,     wg_encode_int(db, cmd_code));
-    wg_set_field(db, cmd_rec, HCQ_CMD_FIELD_SEGID,        wg_encode_int(db, cq->client_segid));  
+    wg_set_field(db, cmd_rec, HCQ_CMD_FIELD_SEGID,        wg_encode_int(db, cq->client.segid));  
     wg_set_field(db, cmd_rec, HCQ_CMD_FIELD_CMD_SIZE,     wg_encode_int(db, data_size));
 
     if (data_size > 0) {
@@ -437,7 +514,7 @@ __cmd_issue(struct cmd_queue * cq,
 
     //printf("Signalling apid %lu\n", cq->apid);
 
-    xemem_signal(cq->apid);
+    xemem_signal(cq->client.apid);
 
     return cmd_id;
 }
@@ -455,6 +532,11 @@ hcq_cmd_issue(hcq_handle_t hcq,
 
     wg_int    lock_id;
 
+    if (cq->type != HCQ_CLIENT) {
+	ERROR("Only clients can issue HCQ commands\n");
+	return HCQ_INVALID_CMD;
+    }
+
     lock_id = wg_start_write(cq->db);
 
     if (!lock_id) {
@@ -471,13 +553,13 @@ hcq_cmd_issue(hcq_handle_t hcq,
     
     /* poll for completion */
     {
-	struct pollfd ufd = {cq->fd, POLLIN, 0};
+	struct pollfd ufd = {cq->client.fd, POLLIN, 0};
 
 	while (hcq_get_cmd_status(hcq, cmd) != HCQ_CMD_RETURNED) {
 	    if (poll(&ufd, 1, -1) == -1) { 
 		ERROR("poll() error\n");
 	    } else {
-		xemem_ack(cq->fd);
+		xemem_ack(cq->client.fd);
 	    }
 	}
     }
@@ -493,7 +575,6 @@ __get_cmd_status(struct cmd_queue * cq,
 {
     hcq_cmd_status_t status = -1;
     void * cmd_rec = NULL;
-
 
     cmd_rec = __get_cmd_rec(cq, cmd);
 
@@ -713,7 +794,7 @@ __get_next_cmd(struct cmd_queue * cq)
     wg_set_field(db, hdr_rec, HCQ_HDR_FIELD_PENDING,     wg_encode_int(db, next_cmd + 1));
 
     /* Quiesce the signal */
-    xemem_ack(cq->fd);
+    xemem_ack(cq->server.fd);
 
     return next_cmd;
 }
@@ -725,6 +806,11 @@ hcq_get_next_cmd(hcq_handle_t hcq)
 
     hcq_cmd_t  cmd  = HCQ_INVALID_CMD;
     wg_int     lock_id;
+
+    if (cq->type != HCQ_SERVER) {
+	ERROR("Only server can retrieve HCQ commands\n");
+	return HCQ_INVALID_CMD;
+    }
 
     lock_id = wg_start_read(cq->db);
 
@@ -880,16 +966,34 @@ __cmd_return(struct cmd_queue * cq,
     wg_set_field(db, cmd_rec, HCQ_CMD_FIELD_STATUS,   wg_encode_int(db, HCQ_CMD_RETURNED));
 
 
-    /* Signal Client segid */
+    /* Signal Client apid */
     {
-	xemem_segid_t segid   = 0;
+	xemem_segid_t segid  = 0;
+	xemem_apid_t  apid   = 0; 
 
 	segid = wg_decode_int(db, wg_get_field(db, cmd_rec, HCQ_CMD_FIELD_SEGID));
-	
-	if (segid > 0) {
-	    //printf("Sending signal to client segid (%lu)\n", segid);
-	    xemem_signal_segid(segid);
+
+	if (segid == 0) 
+	    return 0;
+
+	/* Query connections table for apid */
+	apid = (xemem_apid_t)pet_htable_search(cq->server.connections, (uintptr_t)segid);
+	if (apid == 0) { 
+	    /* Look it up now */
+	    apid = xemem_get(segid, XEMEM_RDWR);
+	    if (apid == -1) {
+		ERROR("Could not find apid for HCQ client: cannot kick client\n");
+		return 0;
+	    } 
+
+	    /* Remember it for future commands issued from this client */
+	    if (pet_htable_insert(cq->server.connections, (uintptr_t)segid, (uintptr_t)apid) == 0) {
+		ERROR("Could not update connections hashtable. This may result in a large "
+			"performance penalty\n");
+	    }
 	}
+
+	xemem_signal(apid);
     }
 
     return 0;
@@ -907,6 +1011,11 @@ hcq_cmd_return(hcq_handle_t hcq,
     struct cmd_queue * cq = hcq;
     wg_int  lock_id;
     int     ret = 0;
+
+    if (cq->type != HCQ_SERVER) {
+	ERROR("Only server can return an HCQ command\n");
+	return -1;
+    }
 
     lock_id = wg_start_write(cq->db);
 
