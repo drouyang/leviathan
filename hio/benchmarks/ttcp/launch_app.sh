453c464ba337ca339ad2bc0574fd506bebbb763b
Jiannan Ouyang <ouyang@cs.pitt.edu>
2016-03-08 16:05:51 -0500

    renamed hio/examples to hio/benchmarks

--- hio/benchmarks/ttcp/launch_app.sh
+++
@@ -1,1 +0,0 @@
-sudo ./hobbes launch_app  --with-hio=/home/ouyang/pisces-modules/leviathan/hio/generic-io-stub/stub --heap_size=64 enclave-1 /home/ouyang/pisces-modules/leviathan/hio/examples/ttcp/ttcp
