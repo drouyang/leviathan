1f24cf4489b192d00c6665426fc20af4b53e2504
Jiannan Ouyang <ouyang@cs.pitt.edu>
2016-04-12 18:13:15 -0400

    hio: syscall forwarding is working on linux with user passed buffer

--- hio/mod/include/hio_ioctl.h
+++ hio/mod/include/hio_ioctl.h
@@ -6,6 +6,8 @@
 #ifndef _HIO_IOCTL_H_
 #define _HIO_IOCTL_H_
 
+#define HIO_ENGINE_MAGIC        0x13131313
+
 // hio commands
 #define HIO_IOCTL_ENGINE_START      3300
 #define HIO_IOCTL_REGISTER          3301 // register stub
