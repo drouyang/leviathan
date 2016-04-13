bc4c2df68e84b7a05b59989b3dcdbdc3bd6c4c44
Jiannan Ouyang <ouyang@cs.pitt.edu>
2016-04-13 15:42:28 -0400

    updated test_xemem

--- hio/mod/src/hio_engine.c
+++ hio/mod/src/hio_engine.c
@@ -227,7 +227,7 @@ int hio_engine_test_syscall(struct hio_engine *engine, struct stub_syscall_t *sy
 
 int hio_engine_init(struct hio_engine *hio_engine) {
 
-    hio_engine->magic = 0x87654321;
+    hio_engine->magic = HIO_ENGINE_MAGIC;
     spin_lock_init(&hio_engine->lock);
     init_waitqueue_head(&hio_engine->syscall_wq);
 
