2e50b9c6b3d06aa85507edb2d7bf7546e3d51f13
Jiannan Ouyang <ouyang@cs.pitt.edu>
2016-04-17 16:49:46 -0400

    added rebuild.sh to rebuild all dependencies

---
+++ rebuild.sh
@@ -0,0 +1,37 @@
+BASE=`pwd`
+
+cd $BASE/petlib
+make clean; make
+
+cd $BASE/petlib/petos
+make clean; make
+
+cd $BASE/kitten/
+make clean; make -j4
+
+cd $BASE/xpmem/mod
+make clean; make
+
+cd $BASE/xpmem/lib
+make clean; make
+
+cd $BASE/pisces/
+make clean; make
+
+cd $BASE/libhobbes
+make clean; make
+
+cd $BASE/shell
+make clean; make
+
+cd $BASE/lnx_inittask
+make clean; make
+
+cd $BASE/lwk_inittask
+make clean; make
+
+cd $BASE/hio/mod
+make clean; make
+
+cd $BASE/hio/mod/stub
+make clean; make
